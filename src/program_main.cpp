67d33d723ee2cac1bafc06e3a58e5046345f2c08
MarixX <144875852+MaR1xXx@users.noreply.github.com>
2024-05-29 11:09:18 +0300

    Update program_main.cpp

--- src/program_main.cpp
+++ src/program_main.cpp
@@ -1,13 +1,6 @@
+#include <main.cpp>
+
 int main(){
-  Player* b = new Forwards("James");
-	b->attack();
-	Player* m = new Center("Maddie");
-	m->attack();
-	Player* ym = new Translator("YaoMing");
-	ym->attack();
-	ym->defense();
-	delete b;
-	delete m;
-	delete ym;
+  RunProgrammMainLogic();
   return 0;
 }
